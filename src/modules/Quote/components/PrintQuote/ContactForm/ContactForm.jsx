e38663531b57d02b3e2a6499ac5833368258ec69
Marcelo De Leon <mdeleon@ragasa.com.mx>
2025-02-11 17:32:46 -0600

    Se implementa envio de correos

--- src/modules/Quote/components/PrintQuote/ContactForm/ContactForm.jsx
+++ src/modules/Quote/components/PrintQuote/ContactForm/ContactForm.jsx
@@ -4,12 +4,25 @@ import Form from "react-bootstrap/Form";
 import CustomButton from "../../../../components/CustomButton/CustomButton";
 import { QuoteContext } from "../../../context/QuoteContext";
 import "./ContactForm.css";
+import { supabase } from "../../../../../supabase/client";
+import emailjs from "emailjs-com";
 
 export default function ContactForm({ formData, handleChange }) {
   const [validated, setValidated] = useState(false);
   const [alertVariant, setAlertVariant] = useState(null);
   const [alertText, setAlertText] = useState("");
-  const { setIsFinished, setActiveStep } = useContext(QuoteContext);
+  const {
+    setIsFinished,
+    setActiveStep,
+    stlFiles,
+    selectedMaterial,
+    selectedInfill,
+    selectedColor,
+    selectedLayerHeight,
+  } = useContext(QuoteContext);
+
+  const [orderId] = useState(createId());
+  const [isSending, setIsSending] = useState(false);
 
   const handleSubmit = (event) => {
     if (event) {
@@ -24,18 +37,99 @@ export default function ContactForm({ formData, handleChange }) {
       } else {
         // EN CASO DE QUE SEA SUCCESS
         setValidated(true);
-        setAlertVariant("success");
-        setActiveStep((prevActiveStep) => prevActiveStep + 1);
-        setIsFinished(true);
-        // setAlertText(
-        //     <>
-        //         Mensaje enviado correctamente, {formData.userName.split(' ')[0]}. En breve le contactaremos para darle seguimiento a su proyecto.
-        //     </>
-        // );
+        setIsSending(true);
+        uploadSupabaseBucket();
       }
     }
     return true;
   };
+
+  const sendAdminEmail = async () => {
+    const req = {
+      user_name: formData.userName,
+      user_email: formData.userEmail,
+      user_phone: formData.userPhone,
+      user_comments: formData.userComments,
+      order_id: orderId,
+      material: selectedMaterial.material,
+      infill: selectedInfill.infill,
+      color: selectedColor.name + " - " + selectedColor.color_hex,
+      layer_height: selectedLayerHeight.layerHeight,
+      stl_files: stlFilesListed,
+    };
+    emailjs
+      .send("service_kmyfgfk", "template_ytn1ffa", req, "k27Yr9noc7Y8QOyoC")
+      .then(
+        function (response) {
+          console.log("SUCCESS!", response.status, response.text);
+          setAlertText(
+            <>
+              Mensaje enviado correctamente, {formData.userName.split(" ")[0]}. En breve
+              le contactaremos para darle seguimiento a su proyecto.
+            </>
+          );
+          setActiveStep((prevActiveStep) => prevActiveStep + 1);
+          setIsFinished(true);
+          setIsSending(false);
+        },
+        function (error) {
+          console.log("FAILED...", error);
+          setAlertVariant("danger");
+          setAlertText(
+            "Hubo un error al procesar su solicitud. Intente de nuevo."
+          );
+        }
+      );
+  };
+
+  const uploadSupabaseBucket = async () => {
+    stlFiles.map(async (stl) => {
+      const { data, error } = await supabase.storage
+        .from("3d_files")
+        .upload(`${orderId}/${stl.file.file.name}`, stl.file.file, {
+          cacheControl: "3600",
+          upsert: false,
+        });
+      if (error) alert("error uploading file");
+      else {
+        sendAdminEmail();
+        console.log(data);
+      }
+    });
+  };
+
+  const stlFilesListed = stlFiles
+    .map((stl) => {
+      if (stl.file.type === "stl") {
+        return `Nombre: ${stl.file.file.name}, X: ${stl.file.width.toFixed(
+          2
+        )}, Y: ${stl.file.height.toFixed(2)}, Z: ${stl.file.depth.toFixed(
+          2
+        )}, ${stl.aspectRatio.toFixed(2)}%, Cantidad: ${stl.quantity}`;
+      } else {
+        return `Nombre: ${stl.file.file.name}, Cantidad: ${stl.quantity}`;
+      }
+    })
+    .join("\n");
+
+  function createId() {
+    const characters =
+      "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    const size = 24;
+    let id = "";
+
+    for (let i = 0; i < size; i++) {
+      const index = Math.floor(Math.random() * characters.length);
+      id += characters.charAt(index);
+    }
+
+    return id;
+  }
+
+  function timeout(number) {
+    return new Promise((res) => setTimeout(res, number));
+  }
+
   return (
     <div>
       <h3>Datos de contacto</h3>
@@ -103,7 +197,7 @@ export default function ContactForm({ formData, handleChange }) {
             Cuéntenos un poco acerca de su proyecto.
           </Form.Control.Feedback>
         </Form.Group>
-        {alertVariant !== "success" && (
+        {(alertVariant !== "success" && isSending === false)  && (
           <CustomButton type="primary">Enviar</CustomButton>
         )}
       </Form>
--- src/modules/Quote/components/PrintQuote/OrderOverview/OrderOverview.jsx
+++ src/modules/Quote/components/PrintQuote/OrderOverview/OrderOverview.jsx
@@ -1,29 +1,25 @@
-import { forwardRef, useContext } from "react";
+import { forwardRef, useContext, useEffect } from "react";
 import { QuoteContext } from "../../../context/QuoteContext";
 import ContactForm from "../ContactForm/ContactForm";
 import "./OrderOverview.css";
 
 const OrderOverview = forwardRef((props, ref) => {
-    const { formData, setFormData } = useContext(QuoteContext);
-    const handleChange = (e) => {
-        const { name, value } = e.target;
-        setFormData({
-            ...formData,
-            [name]: value
-        });
-    };
+  const { formData, setFormData, isFinished } = useContext(QuoteContext);
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
 
-
-    return (
-        <div className="order-overview">
-            <div className="order-overview-container">
-                <ContactForm
-                    formData={formData}
-                    handleChange={handleChange}
-                />
-            </div>
-        </div>
-    );
+  return (
+    <div className="order-overview">
+      <div className="order-overview-container">
+        <ContactForm formData={formData} handleChange={handleChange} />
+      </div>
+    </div>
+  );
 });
 
 export default OrderOverview;
